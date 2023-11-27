63c455c31d1b8fa4db954f6e896e0d4be3b07277
jbritosm <joasabm@gmail.com>
2023-11-27 09:23:12 +0100

    started sudoku

--- EscritorioVirtual/js/memoria.js
+++ EscritorioVirtual/js/memoria.js
@@ -2,85 +2,146 @@ class Memoria {
     constructor() {
         this.elements = {
             "element1": {
-                "nombre": "HTML5",
+                "element": "HTML5",
                 "source": "https://upload.wikimedia.org/wikipedia/commons/3/38/HTML5_Badge.svg"
             },
             "element2": {
-                "nombre": "HTML5",
+                "element": "HTML5",
                 "source": "https://upload.wikimedia.org/wikipedia/commons/3/38/HTML5_Badge.svg"
             },
             "element3": {
-                "nombre": "CSS3",
+                "element": "CSS3",
                 "source": "https://upload.wikimedia.org/wikipedia/commons/6/62/CSS3_logo.svg"
             },
             "element4": {
-                "nombre": "CSS3",
+                "element": "CSS3",
                 "source": "https://upload.wikimedia.org/wikipedia/commons/6/62/CSS3_logo.svg"
             },
             "element5": {
-                "nombre": "JS",
+                "element": "JS",
                 "source": "https://upload.wikimedia.org/wikipedia/commons/b/ba/Javascript_badge.svg"
             },
             "element6": {
-                "nombre": "JS",
+                "element": "JS",
                 "source": "https://upload.wikimedia.org/wikipedia/commons/b/ba/Javascript_badge.svg"
             },
             "element7": {
-                "nombre": "PHP",
+                "element": "PHP",
                 "source": "https://upload.wikimedia.org/wikipedia/commons/2/27/PHP-logo.svg"
             },
             "element8": {
-                "nombre": "PHP",
+                "element": "PHP",
                 "source": "https://upload.wikimedia.org/wikipedia/commons/2/27/PHP-logo.svg"
             },
             "element9": {
-                "nombre": "SVG",
+                "element": "SVG",
                 "source": "https://upload.wikimedia.org/wikipedia/commons/4/4f/SVG_Logo.svg"
             },
             "element10": {
-                "nombre": "SVG",
+                "element": "SVG",
                 "source": "https://upload.wikimedia.org/wikipedia/commons/4/4f/SVG_Logo.svg"
             },
             "element11": {
-                "nombre": "W3C",
+                "element": "W3C",
                 "source": "https://upload.wikimedia.org/wikipedia/commons/5/5e/W3C_icon.svg"
             },
             "element12": {
-                "nombre": "W3C",
+                "element": "W3C",
                 "source": "https://upload.wikimedia.org/wikipedia/commons/5/5e/W3C_icon.svg"
             }             
         }
-        this.hasFlipped = false
+        this.hasFlippedCard = false
         this.lockBoard = false
         this.firstCard = null
         this.secondCard = null
+
+        this.shuffleElements()
+        this.createElements()
+        this.addEventListeners()
     }
 
     shuffleElements() {
         let newJson = {}
 
         let array = Object.keys(this.elements)
-        let currentIndex = array.length,  randomIndex;
-
-        // While there remain elements to shuffle.
-        while (currentIndex > 0) {
-
-            // Pick a remaining element.
-            randomIndex = Math.floor(Math.random() * currentIndex);
-            currentIndex--;
-
-            // And swap it with the current element.
-            [array[currentIndex], array[randomIndex]] = [
-            array[randomIndex], array[currentIndex]];
+        
+        for (let i = array.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
         }
         
-        array.forEach(
-            element => newJson[element] = this.elements[element]
-        )
+        array.forEach(element => newJson[element] = this.elements[element])
 
-        return newJson
+        this.elements = newJson
+    }
+
+    unflipCards() {
+        this.lockBoard = true
+        
+        setTimeout(function() {
+            this.firstCard.dataset.state = "init"
+            this.secondCard.dataset.state = "init"
+            this.resetBoard()
+        }.bind(this), 1000)
+    }
+
+    resetBoard() {
+        this.firstCard = null
+        this.secondCard = null
+        this.hasFlippedCard = false
+        this.lockBoard = false
+    }
+
+    checkForMatch() {
+        console.log(`carta1: ${this.firstCard.dataset.element} carta2: ${this.secondCard.dataset.element}`)
+        let match = this.firstCard.dataset.element == this.secondCard.dataset.element
+        match ? this.disableCards() : this.unflipCards()
+    }
+
+    disableCards() {
+        this.firstCard.dataset.state = "revealed";
+        this.secondCard.dataset.state = "revealed";
+        this.resetBoard();
+    }
+
+    addEventListeners() {
+        let cards = document.getElementsByTagName("article")
+
+        for(let i = 0 ; i < cards.length; i++) {
+            cards[i].onclick = this.flipCard.bind(cards[i], this)
+        }
+    }
+
+    flipCard(game) {
+        if (this.dataset.state === "revealed" || game.lockBoard)
+            return
+        if (game.firstCard == this)
+            return
+
+        this.dataset.state = "flip"
+
+        if (game.hasFlippedCard === true) {
+            game.secondCard = this
+            game.checkForMatch()
+        } else {
+            game.hasFlippedCard = true
+            game.firstCard = this
+        }
+    }
+
+    createElements() { 
+        let array = Object.keys(this.elements)       
+        let section = document.querySelector("body > section:last-of-type")
+        let content = ""
+        for (let key in array) {            
+            content += `<article data-element=\"${this.elements[array[key]]["element"]}\">\n`
+            content += "\t<h3>Tarjeta de memoria</h3>\n"
+            content += `\t<img src=\"${this.elements[array[key]]["source"]}\" alt=\"${this.elements[array[key]]["element"]}\"/>\n`
+            content += "</article>\n"            
+        }
+        section.innerHTML += content
     }
 }
 
 var memoria = new Memoria()
-console.log(memoria.elements)
+
---
+++ EscritorioVirtual/js/sudoku.js
@@ -0,0 +1,71 @@
+class Sudoku {
+    constructor(cadena) {
+        this.tablero = cadena
+        this.rows = 9
+        this.columns = 9
+        this.tableroArray = [...Array(this.rows)].map(e => Array(this.columns))
+        this.start()
+    }
+
+    start() {
+        // Esta variable hace referencia al indice en la cadena
+        let index = 0
+        for(let i = 0; i < this.rows; i++) {
+            for(let j = 0; j < this.columns; j++) {
+                if(this.tablero[index] != '.') {
+                    this.tableroArray[i][j] = parseInt(this.tablero[index])
+                } else {
+                    this.tableroArray[i][j] = 0
+                }
+                index++
+            }
+        }
+    }
+
+    /*
+        Este metodo genera parrafos que representan las celdas
+        del Sudoku.  
+    */
+    createStructure() {
+        let main = document.getElementsByTagName("main")[0]
+        
+        for(let i = 0; i < this.rows * this.columns; i++) {
+            main.innerHTML += "\t<p></p>\n"
+        }
+    }
+
+    /*
+        Este metodo rellena cada uno de esos parrafos
+    */
+    paintSudoku() {
+        this.createStructure()
+        let paragraphs = document.getElementsByTagName("p")
+
+        // Esta variable hace referencia al parrafo a modificar
+        let pCounter = 0
+        for(let i = 0; i < this.rows; i++) {
+            for(let j = 0; j < this.columns; j++) {
+                if(this.tableroArray[i][j] === 0) {
+                    /*
+                        Cada vez que el elemento "p" designado
+                        se clique con el puntero del raton la funcion 
+                        anonima se ejecutara cambiando el "state" de dicho
+                        elemento a "clicked"
+                    */
+                    paragraphs[pCounter].onclick = function() {
+                        this.dataset.state = "clicked"
+                    }.bind(paragraphs[pCounter], this)
+                } else {
+                    paragraphs[pCounter].innerHTML = this.tableroArray[i][j]
+                    paragraphs[pCounter].dataset.state = "blocked"
+                }
+                pCounter++
+            }
+        }
+    }
+
+    introduceNumber(key) {
+        
+    } 
+}
+
